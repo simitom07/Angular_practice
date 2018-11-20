4264bc5ace636854e7be875cebcf0b231b80fd2b
simi.tom <simi.tom@algorisys.com>
2018-11-20 18:16:28 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'X-Men';
+  subtitle ='Mutant and Proud';
+}
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,19 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+        { id: 11, name: 'Wolverine',fullImagePath: "https://vignette.wikia.nocookie.net/x-men/images/c/c3/Wolverine-x-men-evolution-4515780-461-500.gif/revision/latest?cb=20111001234111" },
+        { id: 12, name: 'Magneto',fullImagePath: "https://ae01.alicdn.com/kf/HTB11KQoo4rI8KJjy0Fpq6z5hVXaj/X-hombres-magneto-variante-acci-n-figura-1-8-escala-pintada-figura-anime-ver-Series-6.jpg_640x640.jpg" },
+        { id: 13, name: 'Prof.X',fullImagePath: "http://pm1.narvii.com/5877/bd848c85157088cca3c94c1e89243a851ebbb308_00.jpg" },
+        { id: 14, name: 'Jean Grey',fullImagePath: "https://cdn.shopify.com/s/files/1/0951/5836/products/STL067029.jpg?v=1507841382" },
+        { id: 15, name: 'Rogue',fullImagePath: "https://pre00.deviantart.net/e7eb/th/pre/f/2016/257/4/e/x_men___rogue_by_thetakemi-dahmn65.png" },
+        { id: 16, name: 'Cyclops',fullImagePath: "https://www.usajacket.com/blog/wp-content/uploads/2017/09/Cyclops-X-Men-Scott-Summers-Costume.jpg" },
+        { id: 17, name: 'Storm',fullImagePath: "http://www.adherents.com/lit/comics/img/s/Storm.jpg" },
+        { id: 18, name: 'Beast',fullImagePath: "https://vignette.wikia.nocookie.net/xmenlegends/images/d/d7/Xmenbeast3xmenlegends_2.jpg/revision/latest?cb=20100622051628" },
+        { id: 19, name: 'Mystique',fullImagePath: "https://i.pinimg.com/originals/4a/08/a0/4a08a08ffa6f1a655dc43b11d0f8b54d.jpg" },
+        { id: 20, name: 'Pryde',fullImagePath: "http://cdn-static.denofgeek.com/sites/denofgeek/files/styles/main_wide/public/ktty_pryde_movie_tim_miller_x-men.jpg?itok=66eNf0VL" }
+    ];
+    return {heroes};
+  }
+}
